fa069316e7b65de8d78d6758b1e91bc7e29a0e78
LinghaoChan <Linghao_Chan@163.com>
2024-04-02 20:02:15 +0800

    mld/callback

--- mld/callback/progress.py
+++ mld/callback/progress.py
@@ -1,13 +1,15 @@
 import logging
-
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks import Callback
 import psutil
 
 logger = logging.getLogger()
 
-
+# Define a custom callback class for PyTorch Lightning
 class ProgressLogger(Callback):
+    """
+    A custom callback class for PyTorch Lightning that logs progress information during training.
+    """
 
     def __init__(self, metric_monitor: dict, precision: int = 3):
         # Metric to monitor
@@ -16,14 +18,17 @@ class ProgressLogger(Callback):
 
     def on_train_start(self, trainer: Trainer, pl_module: LightningModule,
                        **kwargs) -> None:
+        # Log a message when training starts
         logger.info("Training started")
 
     def on_train_end(self, trainer: Trainer, pl_module: LightningModule,
                      **kwargs) -> None:
+        # Log a message when training ends
         logger.info("Training done")
 
     def on_validation_epoch_end(self, trainer: Trainer,
                                 pl_module: LightningModule, **kwargs) -> None:
+        # Log a message when a validation epoch ends
         if trainer.sanity_checking:
             logger.info("Sanity checking ok.")
 
@@ -32,23 +37,27 @@ class ProgressLogger(Callback):
                            pl_module: LightningModule,
                            padding=False,
                            **kwargs) -> None:
-        metric_format = f"{{:.{self.precision}e}}"
-        line = f"Epoch {trainer.current_epoch}"
+        # Log a message when a training epoch ends
+        metric_format = f"{{:.{self.precision}e}}"  # Format for logging metrics
+        line = f"Epoch {trainer.current_epoch}"     # Start the log line with the epoch number
         if padding:
-            line = f"{line:>{len('Epoch xxxx')}}"  # Right padding
+            line = f"{line:>{len('Epoch xxxx')}}"   # Right padding
         metrics_str = []
 
         losses_dict = trainer.callback_metrics
         for metric_name, dico_name in self.metric_monitor.items():
+            # If the metric is in the dictionary, format it and add it to the log line
             if dico_name in losses_dict:
                 metric = losses_dict[dico_name].item()
                 metric = metric_format.format(metric)
                 metric = f"{metric_name} {metric}"
                 metrics_str.append(metric)
 
+        # If there are no metrics, return
         if len(metrics_str) == 0:
             return
 
+        # Add the current memory usage to the log line
         memory = f"Memory {psutil.virtual_memory().percent}%"
         line = line + ": " + "   ".join(metrics_str) + "   " + memory
         logger.info(line)
