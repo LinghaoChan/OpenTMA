f145173a04d94e287e6ef1af7ad3adf7468eceaf
LinghaoChan <Linghao_Chan@163.com>
2024-04-02 21:35:35 +0800

    mld/transforms/

--- mld/transforms/feats2smpl.py
+++ mld/transforms/feats2smpl.py
@@ -8,40 +8,32 @@ from mld.data.humanml.data.dataset import Text2MotionDatasetV2
 from mld.data.humanml.scripts.motion_process import recover_from_ric
 from mld.data.humanml.utils.plot_script import plot_3d_motion
 
+# Define the skeleton structure using the kinematic chain from paramUtil
 skeleton = paramUtil.t2m_kinematic_chain
-
-# convert humanML3d features to skeleton format for rendering
-# def feats2joints(motion, data_root = '../datasets/humanml3d'):
-#     '''
-#     input: 263 features
-#     output: 22 joints?
-#     '''
-#     mean = torch.from_numpy(np.load(pjoin(data_root, 'Mean.npy')))
-#     std = torch.from_numpy(np.load(pjoin(data_root, 'Std.npy')))
-
-#     motion = motion * std + mean
-#     motion_rec = recover_from_ric(motion, joints_num=22)
-#     # motion_rec = motion_rec * 1.3
-#     return motion_rec
 
 
 def main():
+    # Define paths and parameters
     data_root = '../datasets/humanml3d'
     feastures_path = 'in.npy'
     animation_save_path = 'in.mp4'
 
     fps = 20
+    # Load the mean and standard deviation of the dataset
     mean = np.load(pjoin(data_root, 'Mean.npy'))
     std = np.load(pjoin(data_root, 'Std.npy'))
 
+    # Load the motion features and normalize them using the mean and standard deviation
     motion = np.load(feastures_path)
     motion = motion * std + mean
     motion_rec = recover_from_ric(torch.tensor(motion), 22).cpu().numpy()
-    # with open('in_22.npy', 'wb') as f:
-    #     np.save(f,motion_rec)
+
+    # Scale the recovered motion
     motion_rec = motion_rec * 1.3
+    # Plot and save the 3D motion
     plot_3d_motion(animation_save_path, motion_rec, title='input', fps=fps)
 
 
+# Run the main function if the script is run as the main program
 if __name__ == '__main__':
     main()
