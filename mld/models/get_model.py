464ead4e3bcd78c75668103be13f5a3e565d6110
LinghaoChan <Linghao_Chan@163.com>
2024-04-04 15:27:08 +0800

    mld/models/get_model.pyg

--- mld/models/get_model.py
+++ mld/models/get_model.py
@@ -2,6 +2,17 @@ import importlib
 
 
 def get_model(cfg, datamodule, phase="train"):
+    """
+    Inputs:
+        cfg (Config): The configuration object containing model details.
+        datamodule (DataModule): The data module object for data loading and processing.
+        phase (str): The phase of model training. Default is "train".
+
+    This function returns the model based on the model type specified in the configuration. If the model type is not supported, it raises a ValueError.
+
+    Returns:
+        Model (object): The model object.
+    """
     modeltype = cfg.model.model_type
     if modeltype in ["mld", "temos"]:
         return get_module(cfg, datamodule)
@@ -10,7 +21,16 @@ def get_model(cfg, datamodule, phase="train"):
 
 
 def get_module(cfg, datamodule):
-    # import pdb; pdb.set_trace()
+    """
+    Inputs:
+        cfg (Config): The configuration object containing model details.
+        datamodule (DataModule): The data module object for data loading and processing.
+
+    This function imports the model module based on the model type specified in the configuration, gets the model class from the module, and returns an instance of the model class.
+
+    Returns:
+        Model (object): The model object.
+    """
     modeltype = cfg.model.model_type
     model_module = importlib.import_module(
         f".modeltype.{cfg.model.model_type}", package="mld.models")
