f15ec14b9f1040def5dd52c1b6d19562887d53e7
LinghaoChan <Linghao_Chan@163.com>
2024-04-08 18:09:45 +0800

    Add new files and directories

--- temos.sh
+++ temos.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-#SBATCH -J Uni-TMR
+#SBATCH -J X-TMR
 #SBATCH -p cvr
 #SBATCH -N 1
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:rtx:4
-#SBATCH --mem 80GB
+#SBATCH --cpus-per-task=18
+#SBATCH --gres=gpu:hgx:4
+#SBATCH --mem 300GB
+#SBATCH --qos=preemptive
 
 source activate temos
 
 # python -m train --cfg configs/configs_temos/MotionX-TMR.yaml --cfg_assets configs/assets.yaml --nodebug
-python -m train --cfg configs/configs_temos/UniMocap-TMR.yaml --cfg_assets configs/assets.yaml --nodebug
-# python -m train --cfg configs/configs_temos/H3D-TMR.yaml --cfg_assets configs/assets.yaml --nodebug
+# python -m train --cfg configs/configs_temos/UniMocap-TMR.yaml --cfg_assets configs/assets.yaml --nodebug
+python -m train --cfg configs/configs_temos/H3D-TMR.yaml --cfg_assets configs/assets.yaml --nodebug
+
+
+# find ./  -type d -name "__pycache__" -exec rm -rf {} +
