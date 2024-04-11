d8659354f93a97cf2f2329401d7e9f418c7876b8
LinghaoChan <Linghao_Chan@163.com>
2024-04-11 11:04:42 +0800

    Update tma/data/humanml/utils/paramUtil.py and tma/data/humanml/utils/get_opt.py files

--- tma/data/humanml/utils/plot_script.py
+++ tma/data/humanml/utils/plot_script.py
@@ -1,4 +1,5 @@
 import math
+
 # import cv2
 from textwrap import wrap
 
@@ -29,20 +30,22 @@ def list_cut_average(ll, intervals):
     return ll_new
 
 
-def plot_3d_motion(save_path, joints, title, figsize=(3, 3), fps=120, radius=3, kinematic_tree=skeleton):
-    matplotlib.use('Agg')
+def plot_3d_motion(
+    save_path, joints, title, figsize=(3, 3), fps=120, radius=3, kinematic_tree=skeleton
+):
+    matplotlib.use("Agg")
 
     # title_sp = title.split(' ')
     # if len(title_sp) > 20:
     #     title = '\n'.join([' '.join(title_sp[:10]), ' '.join(title_sp[10:20]), ' '.join(title_sp[20:])])
     # elif len(title_sp) > 10:
     #     title = '\n'.join([' '.join(title_sp[:10]), ' '.join(title_sp[10:])])
-    title = '\n'.join(wrap(title, 20))
+    title = "\n".join(wrap(title, 20))
 
     def init():
         ax.set_xlim3d([-radius / 2, radius / 2])
         ax.set_ylim3d([0, radius])
-        ax.set_zlim3d([-radius / 3., radius * 2 / 3.])
+        ax.set_zlim3d([-radius / 3.0, radius * 2 / 3.0])
         # print(title)
         fig.suptitle(title, fontsize=10)
         ax.grid(b=False)
@@ -53,7 +56,7 @@ def plot_3d_motion(save_path, joints, title, figsize=(3, 3), fps=120, radius=3, 
             [minx, miny, minz],
             [minx, miny, maxz],
             [maxx, miny, maxz],
-            [maxx, miny, minz]
+            [maxx, miny, minz],
         ]
         xz_plane = Poly3DCollection([verts])
         xz_plane.set_facecolor((0.5, 0.5, 0.5, 0.5))
@@ -72,9 +75,23 @@ def plot_3d_motion(save_path, joints, title, figsize=(3, 3), fps=120, radius=3, 
     # colors = ['red', 'blue', 'black', 'red', 'blue',
     #           'darkblue', 'darkblue', 'darkblue', 'darkblue', 'darkblue',
     #           'darkred', 'darkred', 'darkred', 'darkred', 'darkred']
-    colors = ["#DD5A37", "#D69E00", "#B75A39", "#DD5A37", "#D69E00",
-              "#FF6D00", "#FF6D00", "#FF6D00", "#FF6D00", "#FF6D00",
-              "#DDB50E", "#DDB50E", "#DDB50E", "#DDB50E", "#DDB50E", ]
+    colors = [
+        "#DD5A37",
+        "#D69E00",
+        "#B75A39",
+        "#DD5A37",
+        "#D69E00",
+        "#FF6D00",
+        "#FF6D00",
+        "#FF6D00",
+        "#FF6D00",
+        "#FF6D00",
+        "#DDB50E",
+        "#DDB50E",
+        "#DDB50E",
+        "#DDB50E",
+        "#DDB50E",
+    ]
 
     frame_number = data.shape[0]
     #     print(dataset.shape)
@@ -86,46 +103,41 @@ def plot_3d_motion(save_path, joints, title, figsize=(3, 3), fps=120, radius=3, 
     data[..., 0] -= data[:, 0:1, 0]
     data[..., 2] -= data[:, 0:1, 2]
 
-    #     print(trajec.shape)
-
     def update(index):
-        #         print(index)
         # ax.lines = []
         # ax.collections = []
         ax.view_init(elev=120, azim=-90)
         ax.dist = 7.5
         #         ax =
-        plot_xzPlane(MINS[0] - trajec[index, 0], MAXS[0] - trajec[index, 0], 0, MINS[2] - trajec[index, 1],
-                     MAXS[2] - trajec[index, 1])
-        #         ax.scatter(dataset[index, :22, 0], dataset[index, :22, 1], dataset[index, :22, 2], color='black', s=3)
-
-        # if index > 1:
-        #     ax.plot3D(trajec[:index, 0] - trajec[index, 0], np.zeros_like(trajec[:index, 0]),
-        #               trajec[:index, 1] - trajec[index, 1], linewidth=1.0,
-        #               color='blue')
-        # #             ax = plot_xzPlane(ax, MINS[0], MAXS[0], 0, MINS[2], MAXS[2])
+        plot_xzPlane(
+            MINS[0] - trajec[index, 0],
+            MAXS[0] - trajec[index, 0],
+            0,
+            MINS[2] - trajec[index, 1],
+            MAXS[2] - trajec[index, 1],
+        )
 
         for i, (chain, color) in enumerate(zip(kinematic_tree, colors)):
-            #             print(color)
             if i < 5:
                 linewidth = 4.0
             else:
                 linewidth = 2.0
-            ax.plot3D(data[index, chain, 0], data[index, chain, 1], data[index, chain, 2], linewidth=linewidth,
-                      color=color)
-        #         print(trajec[:index, 0].shape)
+            ax.plot3D(
+                data[index, chain, 0],
+                data[index, chain, 1],
+                data[index, chain, 2],
+                linewidth=linewidth,
+                color=color,
+            )
 
-        plt.axis('off')
+        plt.axis("off")
         ax.set_xticklabels([])
         ax.set_yticklabels([])
         ax.set_zticklabels([])
 
-    ani = FuncAnimation(fig, update, frames=frame_number,
-                        interval=1000 / fps, repeat=False)
+    ani = FuncAnimation(
+        fig, update, frames=frame_number, interval=1000 / fps, repeat=False
+    )
 
-    # writer = FFMpegFileWriter(fps=fps)
     ani.save(save_path, fps=fps)
-    # ani = FuncAnimation(fig, update, frames=frame_number, interval=1000 / fps, repeat=False, init_func=init)
-    # ani.save(save_path, writer='pillow', fps=1000 / fps)
-
     plt.close()
--- tma/data/humanml/utils/word_vectorizer.py
+++ tma/data/humanml/utils/word_vectorizer.py
@@ -3,43 +3,126 @@ import pickle
 from os.path import join as pjoin
 
 POS_enumerator = {
-    'VERB': 0,
-    'NOUN': 1,
-    'DET': 2,
-    'ADP': 3,
-    'NUM': 4,
-    'AUX': 5,
-    'PRON': 6,
-    'ADJ': 7,
-    'ADV': 8,
-    'Loc_VIP': 9,
-    'Body_VIP': 10,
-    'Obj_VIP': 11,
-    'Act_VIP': 12,
-    'Desc_VIP': 13,
-    'OTHER': 14,
+    "VERB": 0,
+    "NOUN": 1,
+    "DET": 2,
+    "ADP": 3,
+    "NUM": 4,
+    "AUX": 5,
+    "PRON": 6,
+    "ADJ": 7,
+    "ADV": 8,
+    "Loc_VIP": 9,
+    "Body_VIP": 10,
+    "Obj_VIP": 11,
+    "Act_VIP": 12,
+    "Desc_VIP": 13,
+    "OTHER": 14,
 }
 
-Loc_list = ('left', 'right', 'clockwise', 'counterclockwise', 'anticlockwise', 'forward', 'back', 'backward',
-            'up', 'down', 'straight', 'curve')
+Loc_list = (
+    "left",
+    "right",
+    "clockwise",
+    "counterclockwise",
+    "anticlockwise",
+    "forward",
+    "back",
+    "backward",
+    "up",
+    "down",
+    "straight",
+    "curve",
+)
 
-Body_list = ('arm', 'chin', 'foot', 'feet', 'face', 'hand', 'mouth', 'leg', 'waist', 'eye', 'knee', 'shoulder', 'thigh')
+Body_list = (
+    "arm",
+    "chin",
+    "foot",
+    "feet",
+    "face",
+    "hand",
+    "mouth",
+    "leg",
+    "waist",
+    "eye",
+    "knee",
+    "shoulder",
+    "thigh",
+)
 
-Obj_List = ('stair', 'dumbbell', 'chair', 'window', 'floor', 'car', 'ball', 'handrail', 'baseball', 'basketball')
+Obj_List = (
+    "stair",
+    "dumbbell",
+    "chair",
+    "window",
+    "floor",
+    "car",
+    "ball",
+    "handrail",
+    "baseball",
+    "basketball",
+)
 
-Act_list = ('walk', 'run', 'swing', 'pick', 'bring', 'kick', 'put', 'squat', 'throw', 'hop', 'dance', 'jump', 'turn',
-            'stumble', 'dance', 'stop', 'sit', 'lift', 'lower', 'raise', 'wash', 'stand', 'kneel', 'stroll',
-            'rub', 'bend', 'balance', 'flap', 'jog', 'shuffle', 'lean', 'rotate', 'spin', 'spread', 'climb')
+Act_list = (
+    "walk",
+    "run",
+    "swing",
+    "pick",
+    "bring",
+    "kick",
+    "put",
+    "squat",
+    "throw",
+    "hop",
+    "dance",
+    "jump",
+    "turn",
+    "stumble",
+    "dance",
+    "stop",
+    "sit",
+    "lift",
+    "lower",
+    "raise",
+    "wash",
+    "stand",
+    "kneel",
+    "stroll",
+    "rub",
+    "bend",
+    "balance",
+    "flap",
+    "jog",
+    "shuffle",
+    "lean",
+    "rotate",
+    "spin",
+    "spread",
+    "climb",
+)
 
-Desc_list = ('slowly', 'carefully', 'fast', 'careful', 'slow', 'quickly', 'happy', 'angry', 'sad', 'happily',
-             'angrily', 'sadly')
+Desc_list = (
+    "slowly",
+    "carefully",
+    "fast",
+    "careful",
+    "slow",
+    "quickly",
+    "happy",
+    "angry",
+    "sad",
+    "happily",
+    "angrily",
+    "sadly",
+)
 
 VIP_dict = {
-    'Loc_VIP': Loc_list,
-    'Body_VIP': Body_list,
-    'Obj_VIP': Obj_List,
-    'Act_VIP': Act_list,
-    'Desc_VIP': Desc_list,
+    "Loc_VIP": Loc_list,
+    "Body_VIP": Body_list,
+    "Obj_VIP": Obj_List,
+    "Act_VIP": Act_list,
+    "Desc_VIP": Desc_list,
 }
 
 
@@ -48,14 +131,15 @@ class WordVectorizer(object):
 
         self.text_encode_way = text_encode_way
 
-        vectors = np.load(pjoin(meta_root, '%s_data.npy'%prefix))
-        words = pickle.load(open(pjoin(meta_root, '%s_words.pkl'%prefix), 'rb'))
-        word2idx = pickle.load(open(pjoin(meta_root, '%s_idx.pkl'%prefix), 'rb'))
+        vectors = np.load(pjoin(meta_root, "%s_data.npy" % prefix))
+        words = pickle.load(open(pjoin(meta_root, "%s_words.pkl" % prefix), "rb"))
+        word2idx = pickle.load(open(pjoin(meta_root, "%s_idx.pkl" % prefix), "rb"))
         self.word2vec = {w: vectors[word2idx[w]] for w in words}
 
-        if 'glove_6B' in self.text_encode_way:
+        if "glove_6B" in self.text_encode_way:
             from torchtext.vocab import GloVe
-            glove_6b = GloVe(name='6B', dim=300)
+
+            glove_6b = GloVe(name="6B", dim=300)
             self.word2vec_glove_6b = glove_6b.get_vecs_by_tokens
 
     def _get_pos_ohot(self, pos):
@@ -63,15 +147,15 @@ class WordVectorizer(object):
         if pos in POS_enumerator:
             pos_vec[POS_enumerator[pos]] = 1
         else:
-            pos_vec[POS_enumerator['OTHER']] = 1
+            pos_vec[POS_enumerator["OTHER"]] = 1
         return pos_vec
 
     def __len__(self):
         return len(self.word2vec)
 
     def __getitem__(self, item):
-        word, pos = item.split('/')
-        if 'given_glove' in self.text_encode_way:
+        word, pos = item.split("/")
+        if "given_glove" in self.text_encode_way:
             if word in self.word2vec:
                 word_vec = self.word2vec[word]
                 vip_pos = None
@@ -84,10 +168,10 @@ class WordVectorizer(object):
                 else:
                     pos_vec = self._get_pos_ohot(pos)
             else:
-                word_vec = self.word2vec['unk']
-                pos_vec = self._get_pos_ohot('OTHER')
-                
-        elif 'glove_6B' in self.text_encode_way:
+                word_vec = self.word2vec["unk"]
+                pos_vec = self._get_pos_ohot("OTHER")
+
+        elif "glove_6B" in self.text_encode_way:
             word_vec = self.word2vec_glove_6b([word]).squeeze()
 
             if word in self.word2vec:
@@ -101,25 +185,25 @@ class WordVectorizer(object):
                 else:
                     pos_vec = self._get_pos_ohot(pos)
             else:
-                pos_vec = self._get_pos_ohot('OTHER')
+                pos_vec = self._get_pos_ohot("OTHER")
 
-
-        
         return word_vec, pos_vec
+
 
 class WordVectorizer_only_text_token(object):
     def __init__(self, meta_root, prefix, text_encode_way):
-        
+
         self.text_encode_way = text_encode_way
 
-        vectors = np.load(pjoin(meta_root, '%s_data.npy'%prefix))
-        words = pickle.load(open(pjoin(meta_root, '%s_words.pkl'%prefix), 'rb'))
-        word2idx = pickle.load(open(pjoin(meta_root, '%s_idx.pkl'%prefix), 'rb'))
+        vectors = np.load(pjoin(meta_root, "%s_data.npy" % prefix))
+        words = pickle.load(open(pjoin(meta_root, "%s_words.pkl" % prefix), "rb"))
+        word2idx = pickle.load(open(pjoin(meta_root, "%s_idx.pkl" % prefix), "rb"))
         self.word2vec = {w: vectors[word2idx[w]] for w in words}
 
-        if 'glove_6B' in self.text_encode_way:
+        if "glove_6B" in self.text_encode_way:
             from torchtext.vocab import GloVe
-            glove_6b = GloVe(name='6B', dim=300)
+
+            glove_6b = GloVe(name="6B", dim=300)
             self.word2vec_glove_6b = glove_6b.get_vecs_by_tokens
 
     def __len__(self):
@@ -127,17 +211,14 @@ class WordVectorizer_only_text_token(object):
 
     def __getitem__(self, item):
         word = item
-        
-        if 'given_glove' in self.text_encode_way:
+
+        if "given_glove" in self.text_encode_way:
             if word in self.word2vec:
                 word_vec = self.word2vec[word]
             else:
-                word_vec = self.word2vec['unk']
-                
-        elif 'glove_6B' in self.text_encode_way:
+                word_vec = self.word2vec["unk"]
+
+        elif "glove_6B" in self.text_encode_way:
             word_vec = self.word2vec_glove_6b([word]).squeeze()
 
         return word_vec
-
-
-
